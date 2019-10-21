846bbd5741e816fc9bf1cbd704ece45a6e042c09
Alexander Torrijos <artorrijos@gmail.com>
2019-10-21 22:35:31 +0800

    DesignerCSharp added

---
+++ DesignerCSharp/DesignerCSharp.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DesignerCSharp
+{
+    partial class DesignerCSharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.ShowMessageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(12, 65);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(39, 13);
+            this.MessageLabel.TabIndex = 0;
+            this.MessageLabel.Text = "[Label]";
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(13, 13);
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(200, 20);
+            this.MessageTextBox.TabIndex = 1;
+            // 
+            // ShowMessageButton
+            // 
+            this.ShowMessageButton.Location = new System.Drawing.Point(13, 39);
+            this.ShowMessageButton.Name = "ShowMessageButton";
+            this.ShowMessageButton.Size = new System.Drawing.Size(200, 23);
+            this.ShowMessageButton.TabIndex = 2;
+            this.ShowMessageButton.Text = "Show Message";
+            this.ShowMessageButton.UseVisualStyleBackColor = true;
+            this.ShowMessageButton.Click += new System.EventHandler(this.ShowMessageButton_Click);
+            // 
+            // DesignerCSharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.ShowMessageButton);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.MessageLabel);
+            this.Name = "DesignerCSharp";
+            this.Text = "DesignerCSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.Button ShowMessageButton;
+    }
+}
+
