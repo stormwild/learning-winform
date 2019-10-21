9ac2ded5bf8cc54f0d46b285d0f3315e03aa6667
Alexander Torrijos <artorrijos@gmail.com>
2019-10-21 22:41:54 +0800

    Designer: InitializeComponent() is generated when a constructor is created

--- Designer/Designer.vb
+++ Designer/Designer.vb
@@ -1,4 +1,13 @@
 ﻿Public Class Designer
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
     Private Sub ShowMessageButton_Click(sender As Object, e As EventArgs) Handles ShowMessageButton.Click
         MessageLabel.Text = MessageTextBox.Text
         MessageBox.Show("Button Clicked")
