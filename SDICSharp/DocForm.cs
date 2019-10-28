942fb8abdea87951745e1a9f1496d756969ebb0e
Alexander Torrijos <artorrijos@gmail.com>
2019-10-28 09:20:12 +0800

    SDI CSharp Sample: working

---
+++ SDICSharp/DocForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDICSharp
+{
+    public partial class DocForm : Form
+    {
+        private static int _counter = 0;
+
+        public DocForm()
+        {
+            InitializeComponent();
+        }
+
+        public static DocForm CreateForm()
+        {
+            var form = new DocForm
+            {
+                Text = "New Document " + ++_counter
+            };
+            SdiApplication.Instance.ApplicationContext.MainForm = form;
+            form.Show();
+            return form;
+        }
+    }
+}
---
+++ SDICSharp/SDIApplication.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDICSharp
+{
+    public class SdiApplication : WindowsFormsApplicationBase
+    {
+        private static SdiApplication _instance = null;
+        
+        public SdiApplication()
+        {
+            IsSingleInstance = true;
+            ShutdownStyle = ShutdownMode.AfterAllFormsClose;
+        }
+
+        public static SdiApplication Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SdiApplication();
+                }
+
+                return _instance;
+            }
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            DocForm.CreateForm();
+        }
+
+        protected override void OnStartupNextInstance(StartupNextInstanceEventArgs eventArgs)
+        {
+            DocForm.CreateForm();
+        }
+    }
+}
